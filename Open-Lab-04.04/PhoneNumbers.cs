d73d0230e1fc7803759ee54ef5f7026ecdb44eab
Jakub Dipčár <jakub.dipcar@gmail.com>
2020-11-10 23:45:57 +0100

    cheese

--- Open-Lab-04.04/PhoneNumbers.cs
+++ Open-Lab-04.04/PhoneNumbers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Principal;
 
 namespace Open_Lab_04._04
 {
@@ -6,7 +7,30 @@ namespace Open_Lab_04._04
     {
         public string FormatPhoneNumber(int[] numbers)
         {
-            throw new NotImplementedException();
+            char[] first = new char[3];
+            char[] second = new char[3];
+            char[] third = new char[4];
+            for (int o = 0; o < 3; o++)
+            {
+                char charnow = Convert.ToChar(numbers[o]+48);
+                first[o] = charnow;
+            }
+            for (int o = 0; o < 3; o++)
+            {
+                char charnow = Convert.ToChar(numbers[o+3]+48);
+                second[o] = charnow;
+            }
+            for (int o = 0; o < 4; o++)
+            {
+                char charnow = Convert.ToChar(numbers[o+6]+48);
+                third[o] = charnow;
+            }
+            string firsti = new string(first);
+            string secondi = new string(second);
+            string thirdi = new string(third);
+            string pepega = ("(" + firsti + ") " + secondi + "-" + thirdi);
+            return pepega;
+
         }
     }
 }
